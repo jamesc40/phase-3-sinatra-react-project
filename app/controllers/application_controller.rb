dedff6be599e4bb0393446cc0a87e2a5a38a67b4
James Coffman <jamesc4023@gmail.com>
2022-05-31 14:27:19 -0400

    login functinality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,25 @@
 require 'pry'
+#enable :sessions
 
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   get "/" do
-    { message: "Good luck with your project!" }.to_json
   end
 
   post '/login' do
-    username = params[:username]
-    password = params[:password]
-    User.exists?(username: username) ?
-      User.login(username, password).to_json : 'invalid username'.to_json
+    user = User.find_by(username: params[:username], 
+                        password: params[:password])
+    if user 
+      true.to_json
+    else
+      status 400
+      false.to_json
+    end
   end
+
+  #get '/logout' do
+  #end
   
   post '/signup' do
     User.create(username: params[:username],
