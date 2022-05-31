5581016e5f9167dfd0a459f5d98eac26d8fe359e
James Coffman <jamesc4023@gmail.com>
2022-05-31 12:08:32 -0400

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
@@ -12,5 +13,30 @@ class ApplicationController < Sinatra::Base
     User.exists?(username: username) ?
       User.login(username, password).to_json : 'invalid username'.to_json
   end
+  
+  post '/signup' do
+    User.create(username: params[:username],
+                password: params[:password],
+                name: params[:name]
+               )
+  end
 
+  patch '/user/:id' do
+    user = User.find(params[:id])
+    params.each do |key, value|
+      unless key == "id"
+        user.update("#{key}": value)
+      end
+    end
+    "success".to_json
+  end
+
+  delete '/user/:id' do
+    User.find(params[:id]).destroy
+  end
+
+  get '/user/:id' do
+    user = User.find(params[:id]) 
+    user.to_json(only: [:id, :name], include: :exercises)
+  end
 end
